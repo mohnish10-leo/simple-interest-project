f0cb47a88f0fe04b4ac3a1d85ccd96aad0adebbd
Mohni <mohnish1030@gmail.com>
2025-08-13 01:17:45 +0530

    Added simple-interest.sh as per specification

---
+++ simple-interest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple Interest Calculator
+
+echo "Enter Principal:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+
+interest=$((principal * rate * time / 100))
+
+echo "Simple Interest is: $interest"
